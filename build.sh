9eab5a330cfaddd8e45146aa68e0b4791464c8de
Matt Jibson <matt.jibson@gmail.com>
2015-01-09 15:37:41 -0500

    Start building arm binaries

--- build.sh
+++ build.sh
@@ -6,16 +6,29 @@ go get -d -v bosun.org/cmd/bosun
 go run build.go > _config.yml
 cat _config.yml
 
-for GOOS in windows linux darwin; do
+build()
+{
+	export GOOS=$1
+	export GOARCH=$2
 	EXT=""
 	if [ $GOOS = "windows" ]; then
 		EXT=".exe"
 	fi
+	if [ $GOARCH = "arm" ]; then
+		export GOARM=${3-6}
+		EXT="v${GOARM}"
+	fi
+	echo $GOOS $GOARCH $EXT
+	go build -o bosun-$GOOS-$GOARCH$EXT bosun.org/cmd/bosun
+	go build -o scollector-$GOOS-$GOARCH$EXT bosun.org/cmd/scollector
+}
+
+build linux arm 5
+build linux arm 6
+build linux arm 7
+
+for GOOS in windows linux darwin; do
 	for GOARCH in amd64 386; do
-		export GOOS=$GOOS
-		export GOARCH=$GOARCH
-		echo $GOOS $GOARCH
-		go build -o bosun-$GOOS-$GOARCH$EXT bosun.org/cmd/bosun
-		go build -o scollector-$GOOS-$GOARCH$EXT bosun.org/cmd/scollector
+		build $GOOS $GOARCH
 	done
 done
